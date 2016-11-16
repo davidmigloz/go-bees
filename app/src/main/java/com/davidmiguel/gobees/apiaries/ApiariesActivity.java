a85cc2e600303f33d373679d3332b819d55450be
davidmigloz <me@davidmiguel.com>
2016-11-17 00:18:24 +0100

    Add apiaries view #67

---
+++ app/src/main/java/com/davidmiguel/gobees/apiaries/ApiariesActivity.java
@@ -0,0 +1,10 @@
+package com.davidmiguel.gobees.apiaries;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by davidmigloz on 16/11/2016.
+ */
+public class ApiariesActivity extends AppCompatActivity {
+
+}
