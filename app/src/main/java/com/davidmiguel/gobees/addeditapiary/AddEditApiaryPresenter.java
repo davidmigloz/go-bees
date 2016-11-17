79f97b8f9d54da0be5245100d17adbe9bdf9c176
davidmigloz <me@davidmiguel.com>
2016-11-17 16:58:39 +0100

    Add addeditapiary test #70

--- app/src/main/java/com/davidmiguel/gobees/addeditapiary/AddEditApiaryPresenter.java
+++ app/src/main/java/com/davidmiguel/gobees/addeditapiary/AddEditApiaryPresenter.java
@@ -1,10 +1,29 @@
 package com.davidmiguel.gobees.addeditapiary;
 
+import android.support.annotation.NonNull;
+
+import com.davidmiguel.gobees.data.source.cache.ApiariesRepository;
+
 /**
  * Listens to user actions from the UI ApiariesFragment, retrieves the data and updates the
  * UI as required.
  */
 public class AddEditApiaryPresenter implements AddEditApiaryContract.Presenter {
+
+    @NonNull
+    private ApiariesRepository apiariesRepository;
+    @NonNull
+    private AddEditApiaryContract.View addeditapiaryView;
+    private int apiaryId;
+
+    public AddEditApiaryPresenter(@NonNull ApiariesRepository apiariesRepository,
+                                  @NonNull AddEditApiaryContract.View addeditapiaryView,
+                                  int apiaryId) {
+        this.apiariesRepository = apiariesRepository;
+        this.addeditapiaryView = addeditapiaryView;
+        this.apiaryId = apiaryId;
+    }
+
     @Override
     public void saveApiary(String name, String notes) {
 
