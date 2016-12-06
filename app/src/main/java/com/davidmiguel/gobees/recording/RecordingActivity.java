693e1c24b862dc6bae7753404b0153224302cefa
davidmigloz <me@davidmiguel.com>
2016-12-06 22:40:50 +0100

    Add recording detail view #91

---
+++ app/src/main/java/com/davidmiguel/gobees/recording/RecordingActivity.java
@@ -0,0 +1,9 @@
+package com.davidmiguel.gobees.recording;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by davidmigloz on 06/12/2016.
+ */
+public class RecordingActivity extends AppCompatActivity {
+}
