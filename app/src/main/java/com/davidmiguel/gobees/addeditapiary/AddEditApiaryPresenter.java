0e0412bbce3acc915b213169ef6403970b77d202
davidmigloz <me@davidmiguel.com>
2016-12-10 02:06:48 +0100

    Add location chooser #83

--- app/src/main/java/com/davidmiguel/gobees/addeditapiary/AddEditApiaryPresenter.java
+++ app/src/main/java/com/davidmiguel/gobees/addeditapiary/AddEditApiaryPresenter.java
@@ -1,5 +1,6 @@
 package com.davidmiguel.gobees.addeditapiary;
 
+import android.content.Context;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -13,11 +14,7 @@ import com.davidmiguel.gobees.data.source.cache.GoBeesRepository;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationSettingsResult;
-import com.google.android.gms.location.LocationSettingsStatusCodes;
 
 /**
  * Listens to user actions from the UI AddEditApiaryFragment, retrieves the data and updates the
@@ -25,20 +22,23 @@ import com.google.android.gms.location.LocationSettingsStatusCodes;
  */
 class AddEditApiaryPresenter implements AddEditApiaryContract.Presenter,
         GetApiaryCallback, TaskCallback, ConnectionCallbacks, OnConnectionFailedListener,
-        ResultCallback<LocationSettingsResult>, LocationListener {
+        LocationListener {
 
     private GoBeesRepository goBeesRepository;
-    private AddEditApiaryContract.View addeditapiaryView;
+    private AddEditApiaryContract.View view;
+
+    private LocationService locationService;
 
     private long apiaryId;
+    private Location apiaryLocation;
 
     AddEditApiaryPresenter(GoBeesRepository goBeesRepository,
-                           AddEditApiaryContract.View addeditapiaryView,
+                           AddEditApiaryContract.View view,
                            long apiaryId) {
         this.goBeesRepository = goBeesRepository;
-        this.addeditapiaryView = addeditapiaryView;
+        this.view = view;
         this.apiaryId = apiaryId;
-        addeditapiaryView.setPresenter(this);
+        view.setPresenter(this);
     }
 
     @Override
@@ -59,6 +59,32 @@ class AddEditApiaryPresenter implements AddEditApiaryContract.Presenter,
     }
 
     @Override
+    public void toogleLocation(Context context) {
+        if (locationService == null) {
+            // Conect GPS service
+            // TODO check permissions
+            locationService = new LocationService(context, this, this);
+            locationService.connect();
+            view.setLocationIcon(true);
+            view.showSearchingGpsMsg();
+        } else {
+            // Disconect GPS service
+            stopLocation();
+        }
+
+    }
+
+    @Override
+    public void stopLocation() {
+        if (locationService != null) {
+            locationService.stopLocationUpdates(this);
+            locationService.disconnect();
+        }
+        locationService = null;
+        view.setLocationIcon(false);
+    }
+
+    @Override
     public void start() {
         if (!isNewApiary()) {
             populateApiary();
@@ -68,111 +94,154 @@ class AddEditApiaryPresenter implements AddEditApiaryContract.Presenter,
     @Override
     public void onApiaryLoaded(Apiary apiary) {
         // Show apiary data on view
-        if (addeditapiaryView.isActive()) {
-            addeditapiaryView.setName(apiary.getName());
-            addeditapiaryView.setNotes(apiary.getNotes());
+        if (view.isActive()) {
+            // Set name
+            view.setName(apiary.getName());
+            // Set notes
+            view.setNotes(apiary.getNotes());
+            // Set location
+            if (apiary.getLocationLat() != null && apiary.getLocationLong() != null) {
+                apiaryLocation = new Location("");
+                apiaryLocation.setLatitude(apiary.getLocationLat());
+                apiaryLocation.setLongitude(apiary.getLocationLong());
+                view.setLocation(apiaryLocation);
+            }
         }
     }
 
     @Override
     public void onDataNotAvailable() {
         // Show error message
-        if (addeditapiaryView.isActive()) {
-            addeditapiaryView.showEmptyApiaryError();
+        if (view.isActive()) {
+            view.showEmptyApiaryError();
         }
     }
 
     @Override
     public void onSuccess() {
         // Apiary saved successfully -> go back to apiaries activity
-        addeditapiaryView.showApiariesList();
+        view.showApiariesList();
     }
 
     @Override
     public void onFailure() {
         // Error saving apiaries
-        addeditapiaryView.showSaveApiaryError();
+        view.showSaveApiaryError();
     }
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        // When we are connected to Google Play service
+        // Get last known recent location (maybe it's not very accurate)
+        Location lastLocation = locationService.getLastLocation();
+        // Note that this can be NULL if last location isn't already known
+        if (lastLocation != null) {
+            // Show last location
+            updateLocation(lastLocation);
+        }
+        // Begin polling for new location updates
+        locationService.createLocationRequest(this);
+        locationService.startLocationUpdates(this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // When we get a location update
+        if (location != null) {
+            // Show current location
+            updateLocation(location);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        // We don't need to perform any action
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        // Show error msg
+        view.showGpsConnectionError();
+    }
+
+    /**
+     * Checks whether an apiary is new or not.
+     *
+     * @return true/false.
+     */
     private boolean isNewApiary() {
         return apiaryId == AddEditApiaryActivity.NEW_APIARY;
     }
 
+    /**
+     * Create an save a new apiary.
+     *
+     * @param name     apiary name.
+     * @param notes    apiary notes.
+     * @param listener TaskCallback.
+     */
     private void createApiary(final String name, final String notes, final TaskCallback listener) {
         // Get next id
         goBeesRepository.getNextApiaryId(new GoBeesDataSource.GetNextApiaryIdCallback() {
             @Override
             public void onNextApiaryIdLoaded(long apiaryId) {
                 // Create apiary
-                Apiary newApiary =
-                        new Apiary(apiaryId, name, null, null, null, notes, null, null, null);
+                Apiary newApiary = new Apiary();
+                // Set id
+                newApiary.setId(apiaryId);
+                // Set name
+                newApiary.setName(name);
+                // Set location
+                if (apiaryLocation != null) {
+                    newApiary.setLocationLat(apiaryLocation.getLatitude());
+                    newApiary.setLocationLong(apiaryLocation.getLongitude());
+                }
+                // Set notes
+                newApiary.setNotes(notes);
                 // Save it if it is correct
                 if (newApiary.isValidApiary()) {
                     goBeesRepository.saveApiary(newApiary, listener);
                 } else {
-                    addeditapiaryView.showEmptyApiaryError();
+                    view.showEmptyApiaryError();
                 }
             }
         });
     }
 
+    /**
+     * Update and save an apiary.
+     *
+     * @param name  apiary name.
+     * @param notes apiary notes.
+     */
     private void updateApiary(String name, String notes) {
         if (isNewApiary()) {
             throw new RuntimeException("updateApiary() was called but apiary is new.");
         }
+        // TODO refactor createApiary() and updateApiary() (almost same code)
         // Create new apiary with the modifications
-        Apiary editedApiary =
-                new Apiary(apiaryId, name, null, null, null, notes, null, null, null);
+        Apiary editedApiary = new Apiary();
+        // Set id
+        editedApiary.setId(apiaryId);
+        // Set name
+        editedApiary.setName(name);
+        // Set location
+        if (apiaryLocation != null) {
+            editedApiary.setLocationLat(apiaryLocation.getLatitude());
+            editedApiary.setLocationLong(apiaryLocation.getLongitude());
+        }
+        // Set notes
+        editedApiary.setNotes(notes);
         goBeesRepository.saveApiary(editedApiary, this);
     }
 
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-    @Override
-    public void onResult(@NonNull LocationSettingsResult locationSettingsResult) {
-        final Status status = locationSettingsResult.getStatus();
-        //final LocationSettingsStates = locationSettingsResult.getLocationSettingsStates();
-        switch (status.getStatusCode()) {
-            case LocationSettingsStatusCodes.SUCCESS:
-                // All location settings are satisfied. The client can
-                // initialize location requests here.
-                break;
-            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
-                // Location settings are not satisfied, but this can be fixed
-                // by showing the user a dialog.
-//                try {
-//                    // Show the dialog by calling startResolutionForResult(),
-//                    // and check the result in onActivityResult().
-//                    status.startResolutionForResult(
-//                            OuterClass.this,
-//                            REQUEST_CHECK_SETTINGS);
-//                } catch (SendIntentException e) {
-//                    // Ignore the error.
-//                }
-                break;
-            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-                // Location settings are not satisfied. However, we have no way
-                // to fix the settings so we won't show the dialog.
-                break;
-        }
-
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-
+    /**
+     * Update apiary location.
+     *
+     * @param location new location.
+     */
+    private void updateLocation(@NonNull Location location) {
+        apiaryLocation = location;
+        view.setLocation(apiaryLocation);
     }
 }
--- app/src/main/java/com/davidmiguel/gobees/addeditapiary/LocationService.java
+++ app/src/main/java/com/davidmiguel/gobees/addeditapiary/LocationService.java
@@ -1,36 +1,39 @@
 package com.davidmiguel.gobees.addeditapiary;
 
-import android.Manifest;
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
+import android.location.Location;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResult;
 
 /**
  * Location service.
+ * Uses Google Play services location API.
  */
-public class LocationService {
+@SuppressWarnings("MissingPermission")
+class LocationService {
 
-    private static final int UPDATE_INTERVAL = 1000;
+    private static final int UPDATE_INTERVAL = 2000;
+    private static final int FASTEST_INTERVAL = 1000;
 
-    private Context context;
     private GoogleApiClient googleApiClient;
     private LocationRequest locationRequest;
 
-    public LocationService(Context context,
-                           ConnectionCallbacks connectionListener,
-                           OnConnectionFailedListener connectionFailedListener) {
-        this.context = context;
+    /**
+     * Location service constructor.
+     *
+     * @param context                  context.
+     * @param connectionListener       ConnectionCallbacks.
+     * @param connectionFailedListener OnConnectionFailedListener.
+     */
+    LocationService(Context context,
+                    ConnectionCallbacks connectionListener,
+                    OnConnectionFailedListener connectionFailedListener) {
+        // Create an instance of the Google Play services API client
         this.googleApiClient = new GoogleApiClient.Builder(context)
                 .addConnectionCallbacks(connectionListener)
                 .addOnConnectionFailedListener(connectionFailedListener)
@@ -38,44 +41,71 @@ public class LocationService {
                 .build();
     }
 
-    public void connect() {
+    /**
+     * Connects the client to Google Play services.
+     */
+    void connect() {
         if (googleApiClient != null) {
             googleApiClient.connect();
         }
     }
 
-    public void disconnect() {
+    /**
+     * Closes the connection to Google Play services.
+     */
+    void disconnect() {
         if (googleApiClient != null) {
             googleApiClient.disconnect();
         }
     }
 
-    public void createLocationRequest(ResultCallback<LocationSettingsResult> callback) {
-        locationRequest = new LocationRequest();
-        locationRequest.setInterval(UPDATE_INTERVAL);
-        locationRequest.setFastestInterval(UPDATE_INTERVAL);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-                .addLocationRequest(locationRequest);
-        PendingResult<LocationSettingsResult> result =
-                LocationServices.SettingsApi.checkLocationSettings(googleApiClient,
-                        builder.build());
-        result.setResultCallback(callback);
+    /**
+     * Configure parameters for requests to the fused location provider.
+     *
+     * @param callback LocationListener.
+     */
+    void createLocationRequest(LocationListener callback) {
+        locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(UPDATE_INTERVAL)
+                .setFastestInterval(FASTEST_INTERVAL);
+        // TODO check if gps is on and if not show a dialog to the user
+        // TODO (now if it's off, we just use the network signal)
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient,
+                locationRequest, callback);
     }
 
-    @SuppressWarnings("MissingPermission")
-    public void startLocationUpdates(LocationListener listener) {
+    /**
+     * Start requesting location updates.
+     *
+     * @param listener LocationListener.
+     */
+    void startLocationUpdates(LocationListener listener) {
         if (locationRequest != null) {
             LocationServices.FusedLocationApi.requestLocationUpdates(
                     googleApiClient, locationRequest, listener);
         }
     }
 
-    public void stopLocationUpdates(LocationListener listener) {
+    /**
+     * Stop requesting location updates.
+     *
+     * @param listener LocationListener.
+     */
+    void stopLocationUpdates(LocationListener listener) {
         if (locationRequest != null) {
             LocationServices.FusedLocationApi.removeLocationUpdates(
                     googleApiClient, listener);
         }
     }
 
+    /**
+     * Get last known location.
+     *
+     * @return location.
+     */
+    Location getLastLocation() {
+        return LocationServices.FusedLocationApi.getLastLocation(
+                googleApiClient);
+    }
 }
