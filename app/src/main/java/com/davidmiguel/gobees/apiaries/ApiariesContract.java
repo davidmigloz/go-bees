7912346622aef34916a9f85dc80c28082a053439
davidmigloz <me@davidmiguel.com>
2016-12-29 00:05:11 +0100

    Revert "Revert "Implement delete apiary #134""
    This reverts commit 6d823cfbc98205d5833469945317e3ebcdd11b12.

--- app/src/main/java/com/davidmiguel/gobees/apiaries/ApiariesContract.java
+++ app/src/main/java/com/davidmiguel/gobees/apiaries/ApiariesContract.java
@@ -17,12 +17,14 @@ interface ApiariesContract {
 
         /**
          * Displays or hide loading indicator.
+         *
          * @param active true or false.
          */
         void setLoadingIndicator(final boolean active);
 
         /**
          * Shows list of apiaries.
+         *
          * @param apiaries apiaries to show (list cannot be empty).
          */
         void showApiaries(@NonNull List<Apiary> apiaries);
@@ -34,6 +36,7 @@ interface ApiariesContract {
 
         /**
          * Opens activity to show the details of the given apiary.
+         *
          * @param apiaryId apiary to show.
          */
         void showApiaryDetail(long apiaryId);
@@ -52,19 +55,31 @@ interface ApiariesContract {
          * Shows successfully saved message.
          */
         void showSuccessfullySavedMessage();
+
+        /**
+         * Shows successfully deleted message.
+         */
+        void showSuccessfullyDeletedMessage();
+
+        /**
+         * Shows error while deleting apiary message.
+         */
+        void showDeletedErrorMessage();
     }
 
     interface Presenter extends BasePresenter {
 
         /**
          * Shows a snackbar showing whether an apiary was successfully added or not.
+         *
          * @param requestCode request code from the intent.
-         * @param resultCode result code from the intent.
+         * @param resultCode  result code from the intent.
          */
         void result(int requestCode, int resultCode);
 
         /**
          * Load apiaries from repository.
+         *
          * @param forceUpdate force cache update.
          */
         void loadApiaries(boolean forceUpdate);
@@ -76,12 +91,21 @@ interface ApiariesContract {
 
         /**
          * Opens activity to show the details of the given apiary.
+         *
          * @param requestedApiary apiary to show.
          */
         void openApiaryDetail(@NonNull Apiary requestedApiary);
 
+        /**
+         * Deletes given apiary.
+         *
+         * @param apiary apiary to delete.
+         */
+        void deleteApiary(@NonNull Apiary apiary);
+
         // TODO eliminar generar y eliminar datos
         void generateData();
+
         void deleteData();
     }
 }
