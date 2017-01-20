7fb5bedf949343be53877f0bf44dbfa3c63e0416
davidmigloz <me@davidmiguel.com>
2017-01-20 18:48:04 +0100

    Add coverage report to sonarqube #155

--- app/src/test/java/com/davidmiguel/gobees/video/OpenCvBaseTest.java
+++ app/src/test/java/com/davidmiguel/gobees/video/OpenCvBaseTest.java
@@ -22,7 +22,7 @@ import org.opencv.core.Core;
 /**
  * Load OpenCV Java lib for running the tests that depend on it.
  */
-public class OpenCvBaseTest {
+public abstract class OpenCvBaseTest {
     static {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
